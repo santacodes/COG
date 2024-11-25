c363e6fd88af0b61eea623654784f488220e3919
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-11-25 18:41:38 +0530

    changed dir structure

--- server/gdal/gdalbind.go
+++ server/gdal/gdalbind.go
@@ -3,8 +3,8 @@ package COG
 /*
 #cgo pkg-config: hdf5
 #cgo CFLAGS: -I/usr/include/gdal
-#cgo LDFLAGS: -L/usr/lib -lgdal
-#include "../c_bindings/insat_metadata.h"
+#cgo LDFLAGS: -L./build -linsat_bindings -L/usr/lib -lgdal -lhdf5
+#include "c_bindings/insat_metadata.h"
 #include <stdlib.h>
 */
 import "C"
@@ -12,7 +12,7 @@ import (
 	"unsafe"
 )
 
-func GetHDF5MetadataAttributes(filename string) string {
+func GetMetadataAttributes(filename string) string {
 	// Convert Go string to C string
 	cFilename := C.CString(filename)
 	defer C.free(unsafe.Pointer(cFilename))
--- server/main.go
+++ server/main.go
@@ -9,7 +9,7 @@ func main() {
 	// Path to the geospatial file (e.g., GeoTIFF or HDF5 file)
 	filePath := "./3RIMG_04SEP2024_1015_L1B_STD_V01R00.h5"
 	// Get the metadata attributes
-	metadata := COG.GetHDF5MetadataAttributes(filePath)
+	metadata := COG.GetMetadataAttributes(filePath)
 
 	// Print the metadata
 	fmt.Println(metadata)
