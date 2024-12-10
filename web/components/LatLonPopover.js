f62a1b34ca2c70f679f1bbb4cb7ae264e9c6ce4d
RohitP2005 <rohitprivate13@gmail.com>
2024-12-10 08:40:10 +0530

    Spacing corrrected

--- web/components/LatLonPopover.js
+++ web/components/LatLonPopover.js
@@ -61,8 +61,8 @@ function LatLonLogger({ map }) {
                 <div className="mb-10 w-full">
                     <h3 className="text-center text-lg font-semibold underline mb-4">{date}</h3>
                     <div className="flex flex-col items-center justify-center w-full text-gray-700">
-                        <div className="flex flex-col space-y-6 w-full max-w-screen-sm bg-white p-10 rounded-xl ring-8 ring-white ring-opacity-40">
-                            <div className="flex justify-between items-center space-x-8 mb-6 w-full">
+                        <div className="flex flex-col items-center justify-center space-y-6 w-full max-w-screen-sm bg-white p-10 rounded-xl ring-8 ring-white ring-opacity-40">
+                            <div className="flex justify-between items-center space-x-20 ">
                                 <div className="flex-1 text-center">
                                     <h2 className="font-bold text-lg">Time</h2>
                                 </div>
@@ -88,21 +88,21 @@ function LatLonLogger({ map }) {
                             </div>
 
                             {groupedData[date].map((entry, idx) => (
-                                <div key={idx} className="flex justify-between items-center space-x-4 mb-6">
-                                    <div className="w-1/4 text-center">{entry.time}</div>
-                                    <div className="w-1/4 text-center">{entry.t2} °C</div>
-                                    <div className="w-1/4 text-center">{entry.psfc} Pa</div>
-                                    <div className="w-1/4 text-center">{entry.rh2} %</div>
-                                    <div className="w-1/4 text-center">
-                                        {(+entry.rainc + +entry.rainnc).toFixed(2)} mm
+                                <div key={idx} className="flex justify-between items-center space-x-20 ">
+                                    <div className=" text-center">{entry.time}</div>
+                                    <div className=" text-center">{entry.t2}°C</div>
+                                    <div className=" text-center">{entry.psfc}Pa</div>
+                                    <div className=" text-center">{entry.rh2}%</div>
+                                    <div className=" text-center">
                                         <svg className="w-6 h-6 fill-current ml-1" viewBox="0 0 16 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                             <g transform="matrix(1,0,0,1,-4,-2)">
                                                 <path d="M17.66,8L12.71,3.06C12.32,2.67 11.69,2.67 11.3,3.06L6.34,8C4.78,9.56 4,11.64 4,13.64C4,15.64 4.78,17.75 6.34,19.31C7.9,20.87 9.95,21.66 12,21.66C14.05,21.66 16.1,20.87 17.66,19.31C19.22,17.75 20,15.64 20,13.64C20,11.64 19.22,9.56 17.66,8ZM6,14C6.01,12 6.62,10.73 7.76,9.6L12,5.27L16.24,9.65C17.38,10.77 17.99,12 18,14C18.016,17.296 14.96,19.809 12,19.74C9.069,19.672 5.982,17.655 6,14Z" style={{ fillRule: "nonzero" }} />
                                             </g>
                                         </svg>
                                     </div>
-                                    <div className="w-1/4 text-center">{entry.ws10} m/s</div>
-                                    <div className="w-1/4 text-center">{entry.wd10} °</div>
+                                        {(+entry.rainc + +entry.rainnc).toFixed(2)}mm
+                                    <div className=" text-center">{entry.ws10}m/s</div>
+                                    <div className="text-center">{entry.wd10}°</div>
                                 </div>
                             ))}
                         </div>
